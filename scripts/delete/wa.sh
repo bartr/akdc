26220073210820f04e8ea19bd0aa66896ee59ab0
bartr <bartr@microsoft.com>
2022-02-03 12:55:44 -0600

    updated scripts

---
+++ scripts/delete/wa.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+az group delete -y --no-wait -g west-wa-seattle-101
+az group delete -y --no-wait -g west-wa-seattle-102
+az group delete -y --no-wait -g west-wa-seattle-103
+az group delete -y --no-wait -g west-wa-seattle-104
+az group delete -y --no-wait -g west-wa-seattle-105
+az group delete -y --no-wait -g west-wa-spokane-101
+az group delete -y --no-wait -g west-wa-spokane-102
+az group delete -y --no-wait -g west-wa-spokane-103
+az group delete -y --no-wait -g west-wa-spokane-104
+az group delete -y --no-wait -g west-wa-spokane-105
+az group delete -y --no-wait -g west-wa-olympia-101
+az group delete -y --no-wait -g west-wa-olympia-102
+az group delete -y --no-wait -g west-wa-olympia-103
+az group delete -y --no-wait -g west-wa-olympia-104
+az group delete -y --no-wait -g west-wa-olympia-105
+az group delete -y --no-wait -g west-wa-west-101
+az group delete -y --no-wait -g west-wa-west-102
+az group delete -y --no-wait -g west-wa-west-103
+az group delete -y --no-wait -g west-wa-west-104
+az group delete -y --no-wait -g west-wa-west-105
+az group delete -y --no-wait -g west-wa-central-101
+az group delete -y --no-wait -g west-wa-central-102
+az group delete -y --no-wait -g west-wa-central-103
+az group delete -y --no-wait -g west-wa-central-104
+az group delete -y --no-wait -g west-wa-central-105
+az group delete -y --no-wait -g west-wa-east-101
+az group delete -y --no-wait -g west-wa-east-102
+az group delete -y --no-wait -g west-wa-east-103
+az group delete -y --no-wait -g west-wa-east-104
+az group delete -y --no-wait -g west-wa-east-105
